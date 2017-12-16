2c9516577c3566f3e0dd7f1db2632c91e082abfe
mohamed <mohamed.nn90@gmail.com>
2017-12-16 20:24:24 +0200

    Initial commit

---
+++ src/main/java/com/geocoding/CamelGeocoderRoute.java
@@ -0,0 +1,26 @@
+package com.geocoding;
+
+import com.google.code.geocoder.model.GeocodeResponse;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestParamType;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CamelGeocoderRoute extends RouteBuilder{
+
+    @Override
+    public void configure() throws Exception {
+
+        rest("/geocoder").description("Geocoder REST service")
+                .consumes("application/json")
+                .produces("application/json")
+
+                .get().description("Geocoder address lookup").outType(GeocodeResponse.class)
+                .param().name("address").type(RestParamType.query).description("The address to lookup").dataType("string").endParam()
+                .responseMessage().code(200).message("Geocoder successful").endResponseMessage()
+                // call the geocoder to lookup details from the provided address
+                .toD("geocoder:address:${header.address}");
+
+    }
+}
