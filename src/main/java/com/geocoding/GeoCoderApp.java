7733a1eaab2839f79c2c5f3e3038744991c1bb02
mohamed <mohamed.nn90@gmail.com>
2017-12-25 07:31:06 +0200

    added spring profiles (dev, stg, prod)

--- src/main/java/com/geocoding/GeoCoderApp.java
+++ src/main/java/com/geocoding/GeoCoderApp.java
@@ -1,15 +1,38 @@
 package com.geocoding;
 
+import com.geocoding.config.DefaultProfileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
-@RestController
 public class GeoCoderApp {
 
-    public static void main(String[] args){
-        SpringApplication.run(GeoCoderApp.class, args);
+    private final static Logger log = LoggerFactory.getLogger(GeoCoderApp.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(GeoCoderApp.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}\n\t" +
+                        "External: \t{}://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                env.getProperty("server.port"),
+                protocol,
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getActiveProfiles());
     }
 
 }
